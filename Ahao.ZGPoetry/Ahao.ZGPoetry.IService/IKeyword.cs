4132d249e2b13088a03c7c047844a74751c4381e
ahao214 <haochen214@163.com>
2020-04-13 22:14:04 +0800

    关键词类接口

---
+++ Ahao.ZGPoetry/Ahao.ZGPoetry.IService/IKeyword.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ahao.ZGPoetry.IService
+{
+    /// <summary>
+    /// 关键词
+    /// </summary>
+    public interface IKeyword
+    {
+    }
+}
---
+++ Ahao.ZGPoetry/Ahao.ZGPoetry.Service/Keyword.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ahao.ZGPoetry.Service
+{
+    /// <summary>
+    /// 关键词
+    /// </summary>
+    public class Keyword
+    {
+    }
+}
